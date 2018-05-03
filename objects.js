3e0577231decf365253ddc63fb77cd454813dab4
Emmanuel Anderson <manu1526@gmail.com>
2018-05-03 09:52:54 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={artistName:"My Bloody Valentine",songTitle:"Sometimes"};
+function updatePlaylist(playlist){
+  Object.assign({"Slowdive":"Allison"},{"Phil Ochs":"Here's to the State of Mississippi"})
+  return playlist;
+}
+
+
+
+
+
